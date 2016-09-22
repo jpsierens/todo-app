b0994d31b8b63cb1bef45987bcefb2b5c3a358e2
jpsierens <jpsierens1988@gmail.com>
2016-09-22 07:49:49 -0600

    about: github star iframe

--- app/components/About.js
+++ app/components/About.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const About = () =>
-    <div>
-        Just a dummy page to showcase react-router!
+    <div className="about">
+        <h2>Thanks for using this example.</h2>
+        <p>Show some love!</p>
+        <div className="github">
+        	<iframe
+        		src="https://ghbtns.com/github-btn.html?user=jpsierens&repo=todo-app&type=star&size=large"
+        		frameBorder="0"
+				allowTransparency="true"
+				scrolling="0">
+			</iframe>
+        </div>
     </div>;
 
 
